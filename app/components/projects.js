2a9a4b2f70f677d6846760da5e04daac7e931420
Kai-Tay <kaishengtay82@gmail.com>
2024-10-30 03:27:11 +0800

    feat: Added Project Page Animations

--- app/components/projects.js
+++ app/components/projects.js
@@ -1,14 +1,41 @@
 'use client'
 
+import { motion, useTransform , useScroll,} from 'framer-motion';
+import { useState, useEffect } from 'react';
 
 export default function About() {
 
+    const [text, setText] = useState('');
+    const fullText = "We all love them.";
+
+    // Use useScroll to track the page's scroll progress
+    const { scrollYProgress } = useScroll();
+
+    // Transform the scroll progress into a character count
+    const characterCount = useTransform(scrollYProgress, [0.6, 0.8], [0, fullText.length]);
+
+    // Update the displayed text as the scroll progresses
+    useEffect(() => {
+        characterCount.onChange((latest) => {
+            setText(fullText.substring(0, Math.round(latest)));
+        });
+    }, [characterCount]);
+
     return (
-        <div id="projects" className="h-screen w-full flex flex-col items-left sm:justify-center ">
-            <div className="font-bold text-8xl text-center">Projects</div>
-            <div className="text-lg mr-96 my-4">Hi! I’m Kai Sheng, a second-year Software Engineering student at SMU. 
-                I’m passionate about exploring new languages and frameworks through hands-on projects, which I find to be the best way to learn. Outside of tech,
-                 I enjoy capturing moments with my film camera and playing volleyball—both give me a great balance between creativity and staying active.</div>
+        <div id="projects" className="h-screen w-full flex flex-col relative">
+            {/* Header */}
+            <motion.div className="flex flex-col font-bold text-8xl text-center mt-52 sticky top-44">
+                Projects
+                <div className="flex justify-center mt-6 text-lg">{text}</div>
+            </motion.div>
+
+            {/* Gap */}
+            <div className="mb-60"></div>
+
+            {/* Image */}
+            <div className="flex scale-75">
+                <img src="/vscode.png" alt="Emoji Background" />
+            </div>
         </div>
     );
 }
